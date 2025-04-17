b8debe481a9b9e0170939f94fd5ad5f20259067f
ToninoJoya <antoniocaprab@gmail.com>
2025-04-17 15:15:46 +0000

    Cambios hechos

--- src/js/components/Cards.jsx
+++ src/js/components/Cards.jsx
@@ -37,10 +37,10 @@ export const Cards = () => {
                                 className="col-12 col-sm-6 col-md-3 mt-3">
                                 <div className="card">
                                     <img src={elem.image} className="card-img-top rounded-0" alt="..." />
-                                    <div className="card-body">
+                                    <div className="card-body text-center">
                                         <h5 className="card-title">Card title</h5>
                                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                        <a href="#" className="btn btn-primary">Find out more!</a>
                                     </div>
                                 </div>
                             </div>
